5b0452439923d4a5441904829d59be72d905c075
AlphaNodesDev <shyjuss8129@gmail.com>
2023-10-24 08:12:57 +0530

    added qr code scanner

--- functions/handlers/scan_qr.php
+++ functions/handlers/scan_qr.php
@@ -1,3 +1,4 @@
+
 <?php if(isset($_GET['question_id'])){
 
 
@@ -15,4 +16,47 @@ if(isset($_POST['qr_code']))
  }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code Scanner</title>
+    <style>
+        body {
+            margin: 0;
+            overflow: hidden;
+        }
 
+        #preview {
+            object-fit: cover;
+            width: 100%;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <video id="preview" playsinline autoplay></video>
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+
+            scanner.addListener('scan', function (content) {
+                alert('Scanned: ' + content);
+            });
+
+            Instascan.Camera.getCameras().then(function (cameras) {
+                const backCameras = cameras.filter(camera => camera.name.includes('back'));
+                if (backCameras.length > 0) {
+                    scanner.start(backCameras[0]);
+                } else {
+                    alert('No back cameras found.');
+                }
+            }).catch(function (e) {
+                console.error(e);
+            });
+        });
+    </script>
+</body>
+</html>
